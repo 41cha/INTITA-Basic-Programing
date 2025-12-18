e60ee2af4c366aeb720630acd6dcf2531fee1c1c
41cha <vovaa.dzimina@gmail.com>
2025-12-18 10:30:41 +0200

    add Cart  class works

--- OrderProcesser/Cart.py
+++ OrderProcesser/Cart.py
@@ -1,24 +1,29 @@
+
 class Cart():
 
     def __init__(self):
         self.__items = []
-        self.available_products = {
+        
+        self.__available_products = {
+            
             '1': 10,
             '2': 15,
             '3': 8
+            
         }
         
     def add_item(self, item, quantity):
         
         if quantity <= 0:
-            raise ValueError("Кількість товару повинна бути більшою за нуль.")
+            raise ValueError("кількість товару повинна бути більшою за нуль")
+        
+        if item not in self.__available_products:
+            raise ValueError("товар не знайдено")
 
-        if self.available_products[item] < quantity:
-            raise ValueError("Недостатня кількість товару на складі.")
+        if self.__available_products[item] < quantity:
+            raise ValueError("недостатня кількість товару на складі")
         
         self.__items.append((item, quantity))
         
-        print(f"Додано товар {item} у кількості {quantity} до кошика.")
-        
 cart = Cart()
-cart.add_item('1', 14)
+cart.add_item('1', 8)
