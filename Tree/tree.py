0b83a87db98dc885464eb2e9ed0ab6b59b435442
41cha <vovaa.dzimina@gmail.com>
2026-02-23 09:57:16 +0200

    add tree update

--- Tree/tree.py
+++ Tree/tree.py
@@ -43,19 +43,33 @@ class Tree:
                 else:
                     curr_root = curr_root.less
 
+    def find_node(self, value):
+        curr_root = self.root
+        while curr_root:
+            match curr_root.compare(value):
+                case 1:
+                    curr_root = curr_root.more
+                case -1:
+                    curr_root = curr_root.less
+                case 0:
+                    return True
+        return False
 
     def __str__(self):
-        queue = [self.root]
-        result = []
-        while len(queue) > 0:
-            current = queue.pop(0)
-            if not current:
-                continue
-            result.append(str(current.val))
-            queue.append(current.less)
-            queue.append(current.more)
+        padding = ''
+        def recursive_str(node, padding):
+            result = padding + str(node.val) + '\n'
+            padding += '.'
+            if node.less:
+                result += recursive_str(node.less, padding)
 
-        return ' '.join(result)
+            if node.more:
+                result += recursive_str(node.more, padding)
+
+            return result
+
+        return recursive_str(self.root, padding)
+
 
 
 tree = Tree()
@@ -64,4 +78,7 @@ tree.add_node(5)
 tree.add_node(3)
 tree.add_node(7)
 tree.add_node(2)
+tree.add_node(1)
 print(tree)
+print(tree.find_node(8))
+print(tree.find_node(5))
